57d73175671deca8854ecbb942e093c9668e9c6b
JustinLau99 <102766333@students.swinburne.edu.my>
2024-06-19 09:38:13 +0800

    move navbar, header, footer

--- x-corporation-uiCookies/about.php
+++ x-corporation-uiCookies/about.php
@@ -1,31 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>X-Corporation</title>
-    <!-- web-fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <!-- off-canvas -->
-    <link href="css/mobile-menu.css" rel="stylesheet">
-    <!-- font-awesome -->
-    <link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <!-- Style CSS -->
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+    <?php include_once 'include/header.php'?>
 <body>
 <div id="main-wrapper">
 
@@ -50,7 +25,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt=""></a>
+            <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt=""></a>
         </div>
 
 
@@ -79,10 +54,10 @@
             </ul>
 
             <ul class="nav navbar-nav navbar-right main-nav">
-                <li><a href="index.html">Home</a></li>
-                <li class="active"><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li class="active"><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
                 <li class="dropdown m-menu-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Features
                     <span><i class="fa fa-angle-down"></i></span></a>
                     <ul class="dropdown-menu">
@@ -360,76 +335,9 @@
 <!-- /.client-logo -->
 
 
+<!-- footer -->
 <footer class="footer">
-
-    <!-- Footer Widget Section -->
-    <div class="footer-widget-section">
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-sm-4 footer-block">
-                    <div class="footer-widget widget_text">
-                        <div class="footer-logo">
-                            <a href="#"><img src="img/logo.png" alt=""></a>
-                        </div>
-                        <p>Continually matrix cross functional opportunities whereas ethical information. Compellingly
-                            streamline enabled human capital before resource-leveling internal or "organic".</p>
-
-                    </div>
-                </div>
-                <!-- /.col-sm-4 -->
-
-                <div class="col-sm-4 footer-block">
-                    <div class="footer-widget widget_text">
-                        <h3>We work for your profit</h3>
-
-                        <p>Distinctively expedite viral materials rather than out-of-the-box solutions. Credibly empower
-                            revolutionary ROI rather than unique products. Collaboratively maximize principle-centered
-                            ideas before highly efficient data. Phosfluorescently.</p>
-                    </div>
-                </div>
-                <!-- /.col-sm-4 -->
-
-                <div class="col-sm-4 footer-block last">
-                    <div class="footer-widget widget_text">
-                        <h3>Contact Us Today</h3>
-                        <address>
-                            Call Us 666 777 888 OR 111 222 333<br>
-                            Send an Email on <a href="mailto:#">contact@domain.com</a><br>
-                            Visit Us 123 Fake Street- Blla 12358<br>
-                            Fake Kingdom<br>
-                        </address>
-
-                        <ul class="list-inline social-links">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- /.col-sm-4 -->
-            </div>
-        </div>
-    </div>
-    <!-- /.Footer Widget Section -->
-
-    <div class="copyright-section">
-        <div class="container clearfix">
-            <span class="copytext">&copy; X-CORPORATION. All rights reserved. | Design By: <a
-                    href="http://uicookies.com">uiCookies</a></span>
-
-            <ul class="list-inline pull-right">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        <!-- .container -->
-    </div>
-    <!-- .copyright-section -->
+    <?php include_once 'include/footer.php' ?>
 </footer>
 <!-- .footer -->
 
@@ -444,10 +352,10 @@
     <div>
         <div>
             <ul id="menu">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
             </ul>
         </div>
     </div>
