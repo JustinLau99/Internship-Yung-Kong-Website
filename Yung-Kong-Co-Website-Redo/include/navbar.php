02a8ad5b19ca1b923d5946d54dee52ce55e25590
JustinLau99 <102766333@students.swinburne.edu.my>
2024-08-01 10:53:24 +0800

    fix navbar switch

--- Yung-Kong-Co-Website-Redo/include/navbar.php
+++ Yung-Kong-Co-Website-Redo/include/navbar.php
@@ -40,8 +40,7 @@ $currentPage = ($currentPage == 'product-details.php') ? 'products.php' : $curre
 		<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav ms-auto">
 				<?php foreach ($pages as $page): ?>
-
-					<?php switch ($page):
+					<?php switch ($page['url']):
 						case 'products.php':
 							?>
 
@@ -98,57 +97,6 @@ $currentPage = ($currentPage == 'product-details.php') ? 'products.php' : $curre
 							break;
 					endswitch;
 					?>
-
-
-
-					<?php if ($page['url'] === 'products.php'): ?>
-
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle text-white <?= ($currentPage === $page['url']) ? 'nav-active' : ''; ?>"
-								href="products.php" id="navbarDropdown" role="button">
-								<?= htmlspecialchars($page['name']); ?>
-							</a>
-
-							<!-- only run if file exists -->
-							<?php if (file_exists('phpData/product_category.csv') && file_exists('phpData/readCSV.php')): ?>
-								<!-- dropdown: product category -->
-								<ul class="dropdown-menu p-3">
-									<div class="container-fluid">
-										<div class="row g-0">
-											<?php include_once 'phpData/readCSV.php'; ?>
-											<?php $product_category = readCSVData("phpData/product_category.csv") ?>
-											<?php foreach ($product_category as $prodCat): ?>
-												<div class="col-md-2 d-flex align-items-center bg-transparent">
-													<a class="dropdown-item text-wrap fw-bold w-100 h-100 text-shadow"
-														href="products.php#<?= htmlspecialchars($prodCat['img_path']); ?>">
-														<i class="<?= htmlspecialchars($prodCat['icon']); ?>"></i>
-														<?= htmlspecialchars($prodCat['name']); ?>
-													</a>
-												</div>
-											<?php endforeach; ?>
-										</div>
-									</div>
-								</ul>
-							<?php endif; ?>
-						</li>
-
-
-
-					<?php else: ?>
-						<li class="nav-item">
-							<a class="nav-link text-white <?= ($currentPage === $page['url']) ? 'nav-active' : ''; ?>"
-								href="<?= htmlspecialchars($page['url']); ?>">
-								<?= htmlspecialchars($page['name']); ?>
-							</a>
-						</li>
-					<?php endif; ?>
-
-
-
-
-
-
-
 
 				<?php endforeach; ?>
 
